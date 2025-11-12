31bb8d66e512d66d31ae8c3caca05c465d82e0b1
Afees Kolawole <dcodesmith@gmail.com>
2025-11-12 20:30:22 +0000

    chore(cucumber): rip out e2e cucumber completely

--- test/support/support.interface.ts
+++
@@ -1,9 +0,0 @@
-export const Role = {
-  USER: "user",
-  FLEET_OWNER: "fleetOwner",
-  CHAUFFEUR: "chauffeur",
-  STAFF: "staff",
-  ADMIN: "admin",
-} as const;
-
-export type RoleType = (typeof Role)[keyof typeof Role];
